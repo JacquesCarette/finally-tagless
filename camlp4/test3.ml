10bb5c413bf65092eae4ab584858d7b16014047c
Oleg Kiselyov <oleg@okmij.org>
2005-02-24 09:51:45 +0000

    new monad working

--- camlp4/test3.ml
+++ camlp4/test3.ml
@@ -6,15 +6,15 @@
 
 let test_simple_expression () =
     let ret x = x in
-  mdo { mret true }
+  mdo { ret true }
 
-let brt : ('a, int) code = brackets 0 ;;
+let brt : ('a, int) code = .< 0 >. ;;
 
 let test_brackets =
   let ret x = x and bind x f = f x in
-    mdo { y <-- brackets 0 ;
-          mret y }
+    mdo { y <-- .< 0 >. ;
+          ret y }
 
 let test_escape =
-  let x = brackets 1 in
-    brackets (escape x)
+  let x = .< 1 >. in
+    .< .~x >.
