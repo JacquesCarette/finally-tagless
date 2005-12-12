926c5fb892b9be8f6cb06defaa201fb6ebf32edb
Oleg Kiselyov <oleg@okmij.org>
2005-12-12 22:25:03 +0000

    code from Lydia, from her Dec 11th message

--- Makefile
+++ Makefile
@@ -1,9 +1,8 @@
 # name:          Makefile
 # synopsis:      Construction rules for monadic "do" syntax extension
 # author:        Lydia E. van Dijk
-# adapted:       Jacques Carette
-# last revision: Wed Feb 2 2005
-# ocaml version: 3.08.0
+# last revision: Sat Dec 10 08:56:57 UTC 2005
+# ocaml version: 3.09.0
 
 
 ########################################################################
@@ -12,13 +11,14 @@
 #
 ########################################################################
 
-SYNTAX_EXTENSION := pa_monad2.cmo
+SYNTAX_EXTENSION := pa_monad.cmo
 
 PP := camlp4o -I . $(SYNTAX_EXTENSION)
+PP-EXT := camlp4o -I . pa_extend.cmo q_MLast.cmo
 
-TEST := test-monad2 tt
+TEST := test-monad
 
-DISTNAME := monad-function
+DISTNAME := monad-syntax-extension
 
 VERSION := 1.0
 
@@ -30,18 +30,25 @@ VERSION := 1.0
 ########################################################################
 
 .PHONY: all
-all: $(TEST)
+all: $(SYNTAX_EXTENSION)
 
 
 .PHONY: test
-test: all
-	./test-monad2
-	./tt
+test: pythagorean-triples test-monad exception test-syntax
+	for x in $^; do $$x; done
 
 
-.PHONY: pretty-print
-pretty-print: $(TEST)-pretty-print.ml
-	cat $^
+.PHONY: doc
+doc: html-doc/pa_monad.html
+
+
+.PHONY: dist
+dist: distcheck
+	darcs dist --dist-name $(DISTNAME)-$(VERSION)
+
+
+.PHONY: distcheck
+distcheck: distclean test
 
 
 .PHONY: top-level
@@ -51,30 +58,34 @@ top-level: $(SYNTAX_EXTENSION)
 
 .PHONY: clean
 clean:
-	rm --force $(TEST) *.cm[io]
+	rm --force $(TEST) *.cm[io] *.annot
+
+
+.PHONY: distclean
+distclean: clean
+	rm --force *.ml-pp $(DISTNAME)-*.tar.gz *~
 
 
 ########################################################################
 #
-# Explicit and Implicit Rules
+# Implicit Rules
 #
 ########################################################################
 
+# Compile syntax extension
 pa_%.cmo: pa_%.ml
-	ocamlc -c -pp 'camlp4o pa_extend.cmo q_MLast.cmo' -I +camlp4 $<
+	ocamlc -c -dtypes -pp '$(PP-EXT)' -I +camlp4 $<
 
 
-test3.cmo: test3.ml pa_monad.cmo
-	metaocamlc -c -pp 'camlp4o pa_extend.cmo q_MLast.cmo ./pa_monad.cmo' -I +camlp4 $<
-
-%-pretty-print.ml: %.ml
+# Pritty-print a given OCaml file
+%.ml-pp: %.ml pa_monad.cmo
 	$(PP) pr_o.cmo $< > $@
 
-tt: tt.ml test3.cmo
-	metaocamlc ./test3.cmo -o $@ $<
 
-%: %.ml
-	metaocamlc -pp '$(PP)' -o $@ $<
+# Compile an OCaml file to an executable using the syntax extension
+%: %.ml $(SYNTAX_EXTENSION)
+	ocamlc -warn-error A -pp '$(PP)' -o $@ $<
+
 
 ########################################################################
 #
@@ -82,4 +93,6 @@ tt: tt.ml test3.cmo
 #
 ########################################################################
 
-$(TEST): $(SYNTAX_EXTENSION) $(TEST).ml
+html-doc/pa_monad.html: pa_monad.ml
+	test -d html-doc || mkdir html-doc
+	ocamldoc -html -d html-doc -pp '$(PP-EXT)' -I +camlp4 -o $@  $<
--- pa_monad.ml
+++ pa_monad.ml
@@ -2,28 +2,35 @@
  * synopsis:      Haskell-like "do" for monads
  * authors:       Jacques Carette and Oleg Kiselyov,
  *                based in part of work of Lydia E. Van Dijk
- * last revision: Sat Apr 30 07:28:39 UTC 2005
- * ocaml version: 3.08.2 *)
+ * last revision: Sun Dec 11 08:30:44 UTC 2005
+ * ocaml version: 3.09.0 *)
 
 
-(** {2 Conversion Rules}
+(** {2 Syntax Extension to Support Monads}
+
+This module extends OCaml's syntax by a Haskell-like "do"-notation is
+particularly suited for the work with monads.
+
+{2 Conversion Rules}
 
 {3 Grammar informally}
+We support four different constructs to indroduce a monadic expressions.
 {[
         mdo exp
         mdo exp1; exp2
-        mdo x <-- exp; exp
+        mdo x <-- exp1; exp2
         mdo let x = foo in; exp
 ]}
-which is almost literally the grammar of Haskell [do] notation,
-modulo [do]/[mdo] and [<-]/[<--].
+which is almost literally the grammar of the Haskell "do"-notation,
+with the differences that Haskell uses "do" and "<-" where we use
+"[mdo]" and "[<--]".
 
-The actual bind function of the monad defaults to [bind].  To select a
-different function, use the extended form of [mdo]:
+The actual bind function of the monad defaults to "[bind]".  To select a
+different function, use the extended form of "[mdo]":
 {[
         mdo with exp1 in exp2
         mdo with exp1 in exp3; exp4
-        mdo with exp in x <-- exp; exp
+        mdo with exp1 in x <-- exp2; exp3
         mdo with exp in let x = foo in; exp
 ]}
 
@@ -31,9 +38,9 @@ different function, use the extended form of [mdo]:
 {[
         "mdo" ["with" EXP "in"] <do-body>
         <do-body> ::=
-                "let" var "=" EXP ("and" var "=" EXP)* "in" ";" <do-body>
-                EXP
-                (pat "<--")? EXP ";" <do-body>
+                  "let" var "=" EXP ("and" var "=" EXP)* "in" ";" <do-body>
+                | EXP
+                | (pat "<--")? EXP ";" <do-body>
 ]}
 
 {3 Semantics (as re-writing into the core language)}
@@ -53,72 +60,137 @@ The reason we cannot terminate the [let] expression with just a
 semi-colon is because semi-colon can be a part of an expression that
 is bound to the pattern.
 
-It would be possible to use [<-] instead of [<--].  In that case, the
-similarity to the [do] notation of Haskell will be complete.  However,
-due to the parsing rules of Camlp4, we would have to accept [:=] as an
-alias for [<-].  So,
+It would be possible to use "[<-]" instead of "[<--]".  In that case, the
+similarity to the "[do]" notation of Haskell will be complete.  However,
+due to the parsing rules of Camlp4, we would have to accept "[:=]" as an
+alias for "[<-]".  So,
 {[        mdo pat := exp1; exp2]}
 would be allowed too.  Perhaps that is too much.
 
-The major difficulty with the [do] notation is that it cannot truly be
+The major difficulty with the "[do]" notation is that it cannot truly be
 parsed by an LR-grammar.  Indeed, to figure out if we should start
 parsing <do-body> as an expression or a pattern, we have to parse it
-as a pattern and check the [<--] delimiter.  If it is not there, we
+as a pattern and check the "[<--]" delimiter.  If it is not there, we
 should {e backtrack} and parse it again as an expression.
 Furthermore, [a <-- b] (or [a <- b]) can also be parsed as an
 expression.  However, for some patterns, for example ([_ <-- exp]),
 that cannot be parsed as an expression. *)
 
-type monbind =
-    BindL of (MLast.patt * MLast.expr) list
-  | BindM of MLast.patt * MLast.expr
-  | ExpM  of MLast.expr
 
-(* Convert MLast.expr into MLast.patt, if we `accidentally' parsed a
-   pattern as an expression.  The code is based on
-   pattern_eq_expression in pa_fstream.ml. *)
-let rec exp_to_patt loc e =
-  match e with
-      <:expr< $lid:b$ >> -> <:patt< $lid:b$ >>
-    | <:expr< $uid:b$ >> -> <:patt< $uid:b$ >>
-    | <:expr< $e1$ $e2$ >> ->
-      let p1 = exp_to_patt loc e1
-      and p2 = exp_to_patt loc e2 in
+(** Syntactically, we can encouter three different kinds of monadic
+    bindings.  Sometimes we find out about an expression that is not a
+    monadic binding, just an expression; this is the fourth case. *)
+type monadic_binding =
+    BindL of (MLast.patt * MLast.expr) list (** "[let]" inside "[mdo]" *)
+  | BindMRef of MLast.patt * MLast.expr     (** bind ("[>>=]") using a refutable pattern *)
+  | BindMIrref of MLast.patt * MLast.expr   (** bind ("[>>=]") using an irrefutable pattern *)
+  | ThenM of MLast.expr                     (** "then"-binding, this is the "[>>]"-operator *)
+
+
+(** Convert [an_expression] into a pattern, if we "accidentally" parse
+    a pattern as an expression. *)
+(*  The code is based on [pattern_eq_expression] in {i pa_fstream.ml}. *)
+let rec exp_to_patt (_loc: MLast.loc) (an_expression: MLast.expr): MLast.patt =
+  match an_expression with
+      <:expr< $int:s$ >> -> <:patt< $int:s$ >> (* integer constant*)
+    | <:expr< $chr:c$ >> -> <:patt< $chr:c$ >> (* character constant;*)
+    | <:expr< $str:s$ >> -> <:patt< $str:s$ >> (* string constant *)
+    | <:expr< $lid:b$ >> -> <:patt< $lid:b$ >> (* local variable *)
+    | <:expr< $uid:b$ >> -> <:patt< $uid:b$ >> (* variable of other module *)
+    | <:expr< $e1$ $e2$ >> ->                  (* function application *)
+      let p1 = exp_to_patt _loc e1
+      and p2 = exp_to_patt _loc e2 in
         <:patt< $p1$ $p2$ >>
-    | <:expr< ($list:el$) >> ->
-      let pl = List.map (exp_to_patt loc) el in
+    | <:expr< ($list:el$) >> ->                (* tuple *)
+      let pl = List.map (exp_to_patt _loc) el in
         <:patt< ($list:pl$) >>
+    | <:expr< { $list:eel$ } >> ->             (* record *)
+      let ppl = List.map (fun (p, e) -> p, exp_to_patt _loc e) eel in
+        <:patt< { $list:ppl$ } >>
     | _ -> failwith "exp_to_patt: this pattern is not yet supported"
 
-(* The main semantic function *)
-let process loc b f =
-  let globbind1 x acc = <:expr< $f$ $x$ (fun _ -> $acc$) >>
-  and globbind2 x p acc = <:expr< $f$ $x$ (fun $p$ -> $acc$) >>
-  and ret n = <:expr< $n$ >> in
+
+(** Answer whether [a_pattern] is irrefutable. *)
+let rec is_irrefutable_pattern (a_pattern: MLast.patt): bool =
+  match a_pattern with
+      <:patt< { $list:ppl$ } >> ->      (* record *)
+      List.for_all (fun (_p1, p2) -> is_irrefutable_pattern p2) ppl
+    | <:patt< () >> -> true             (* unit *)
+    | <:patt< ( $p$ : $t$ ) >> ->       (* type constraint *)
+      is_irrefutable_pattern p
+    | <:patt< ( $p1$ as $p2$ ) >> ->    (* alias *)
+      is_irrefutable_pattern p1
+    | <:patt< ( $list:pl$ ) >> ->       (* tuple of patterns *)
+      List.for_all is_irrefutable_pattern pl
+    | <:patt< $lid:s$ >> -> true        (* variable *)
+    | <:patt< _ >> -> true              (* wildcard *)
+    | _ -> false
+(** Answer whether [an_expression] represents an irrefutable pattern. *)
+and is_irrefutable_expression (an_expression: MLast.expr): bool =
+  match an_expression with
+      <:expr< { $list:eel$ } >> ->      (* record *)
+      List.for_all (fun (_p, e) -> is_irrefutable_expression e) eel
+    | <:expr< () >> -> true             (* unit *)
+    | <:expr< ( $exp$ : $t$ ) >> ->     (* type constraint *)
+      is_irrefutable_expression exp
+      (* MISSING: parenthesized expression? *)
+    | <:expr< ( $list:el$ ) >> ->
+      List.for_all is_irrefutable_expression el
+    | <:expr< $lid:s$ >> -> true        (* variable *)
+      (* MISSING: wildcard? *)
+    | _ -> false
+
+
+(** Convert all expressions [a_binding_list] inside "mdo" into core
+    OCaml.  Use [a_bind_function] as the monad's "bind"-function. *)
+let convert
+    (_loc: MLast.loc)
+    (a_binding_list: monadic_binding list)
+    (a_bind_function: MLast.expr): MLast.expr =
+  let glob_then x acc = <:expr< $a_bind_function$ $x$ (fun _ -> $acc$) >>
+  and glob_bind_irrefutable x p acc = <:expr< $a_bind_function$ $x$ (fun $p$ -> $acc$) >>
+  and glob_bind_refutable x p acc =
+    <:expr< $a_bind_function$ $x$ (fun [$p$ -> $acc$ | _ -> fail "bind"]) >> in
   let folder =
     fun acc y ->
       match y with
-          BindM (p, x) -> globbind2 x p acc
-        | ExpM x -> globbind1 x acc
+          BindMIrref (p, x) -> glob_bind_irrefutable x p acc
+        | BindMRef (p, x) -> glob_bind_refutable x p acc
+        | ThenM x -> glob_then x acc
         | BindL l -> <:expr< let $list:l$ in $acc$ >>
   in
-    match List.rev b with
-        [] -> failwith "process: somehow got an empty list from a LIST1!"
-      | ExpM n :: t -> List.fold_left folder (ret n) t
-      | _ -> failwith "process: does not end with an expression"
+    match List.rev a_binding_list with
+        [] -> failwith "convert: somehow got an empty list from a LIST1"
+      | ThenM n :: t -> List.fold_left folder <:expr< $n$ >> t
+      | _ -> failwith "convert: does not end with an expression"
+
+(** [attach_bind an_expression]
+
+    Append "[bind]" to the module name given in [an_expression].  Fail
+    if [an_expression] is not a valid module name. *)
+let append_bind _loc an_expression =
+  match an_expression with
+      <:expr< $e1$ . $e2$ >> as m -> <:expr< $m$ . bind >>
+    | <:expr< $uid:s$ >> as m -> <:expr< $m$ . bind >>
+    | _ -> failwith "append_bind: 'with module' expects a module name or module-path."
+
 
 EXTEND
     GLOBAL: Pcaml.expr;
 
     Pcaml.expr: LEVEL "expr1"
     [
+      [ "mdo"; "with"; "module"; monad_module = Pcaml.expr; "in";
+        bindings = LIST1 monadic_binding SEP ";" ->
+          convert _loc bindings (append_bind _loc monad_module) ]
+    |
       [ "mdo"; "with"; bind_fun = Pcaml.expr; "in";
         bindings = LIST1 monadic_binding SEP ";" ->
-          process loc bindings bind_fun ]
+          convert _loc bindings bind_fun ]
     |
       [ "mdo";
         bindings = LIST1 monadic_binding SEP ";" ->
-          process loc bindings (<:expr< bind >>) ]
+          convert _loc bindings <:expr< bind >> ]
     ] ;
 
     Pcaml.expr: BEFORE "apply"
@@ -129,18 +201,50 @@ EXTEND
 
     monadic_binding:
     [
-      [ "let"; l = LIST1 Pcaml.let_binding SEP "and"; "in" ->
-	  BindL(l) ]
+      [ "let"; l = LIST1 Pcaml.let_binding SEP "and"; "in" -> BindL l ]
     |
       [ x = Pcaml.expr LEVEL "expr1" ->
 	(* For some patterns, "patt <-- exp" can parse as an
-	   expression too.  So we have to figure out which is which. *)
+	   expression too.  So we have to figure out which is
+	   which. *)
           match x with
-              <:expr< $e1$  $lid:op$ $e3$ >> when op = "<--" ->
-                    BindM (exp_to_patt loc e1, e3)
-            | _ -> ExpM x ]
+              <:expr< $e1$ $lid:op$ $e3$ >> when op = "<--" ->
+                    if is_irrefutable_expression e1 then
+                       BindMIrref (exp_to_patt _loc e1, e3)
+                    else BindMRef (exp_to_patt _loc e1, e3)
+            | _ -> ThenM x ]
     |
-      [ p = Pcaml.patt LEVEL "simple"; "<--"; x = Pcaml.expr LEVEL "expr1" ->
-          BindM (p, x) ]
+      [ p = ipatt; "<--"; x = Pcaml.expr LEVEL "expr1" ->
+          BindMIrref (p, x)
+      | p = Pcaml.patt LEVEL "simple"; "<--"; x = Pcaml.expr LEVEL "expr1" ->
+          BindMRef (p, x) ]
+    ] ;
+
+    patt_label_ident:
+    [ LEFTA
+      [ p1 = SELF; "."; p2 = SELF -> <:patt< $p1$ . $p2$ >> ]
+    | "simple" RIGHTA
+      [ i = UIDENT -> <:patt< $uid:i$ >>
+      | i = LIDENT -> <:patt< $lid:i$ >> ]
+    ] ;
+
+    label_ipatt:
+    [ [ i = patt_label_ident; "="; p = ipatt -> (i, p) ] ] ;
+
+    (* irrefutable patterns (from "pa_r.ml") *)
+    ipatt:
+    [ [ "{"; lpl = LIST1 label_ipatt SEP ";"; "}" ->
+          <:patt< { $list:lpl$ } >>            (* record *)
+      | "("; ")" -> <:patt< () >>              (* unit *)
+      | "("; p = SELF; ")" ->                  (* parenthesized pattern *)
+          <:patt< $p$ >>
+      (* MISSING: type restriction *)
+      | p = SELF; "as"; p2 = SELF ->           (* alias *)
+          <:patt< ($p$ as $p2$) >>
+      | p = SELF; ","; ps = LIST1 ipatt SEP "," ->
+          let lst = p :: ps in
+            <:patt< ($list:lst$) >>            (* tuple of patterns *)
+      | s = LIDENT -> <:patt< $lid:s$ >>       (* variable *)
+      | "_" -> <:patt< _ >> ]                  (* wildcard *)
     ] ;
 END;
