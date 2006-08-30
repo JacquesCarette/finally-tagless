4616a2fe222c5d374475b5a772bf82d77f1692ba
Oleg Kiselyov <oleg@okmij.org>
2006-08-30 09:38:17 +0000

    small testing module I played with yesterday.

---
+++ TOMS/code/kindcheck.ml
@@ -0,0 +1,67 @@
+module Sig = struct
+  type domain_is_field
+  type domain_is_ring
+  module type DOMAIN = sig
+    type kind
+    type v
+    val z : v
+  end
+  module type COLL = sig
+    module Dom : DOMAIN
+    type coll
+  end
+end
+
+module Doms = struct
+ open Sig
+
+  module FDomain = struct
+    type kind = domain_is_field
+    type v = float
+    let z = 0.0
+  end
+
+  module IDomain = struct
+    type kind = domain_is_ring
+    type v = int
+    let z = 0
+  end
+
+  module GColl(Dom:DOMAIN) =
+    struct
+      module Dom = Dom
+      type coll = Dom.v list
+    end
+end
+;;
+
+module GEF = struct
+  open Sig
+  module DivisionUpdate 
+      (C:COLL with type Dom.kind = domain_is_field) = struct
+	module Dom = C.Dom
+	open Dom
+	let update x = x
+  end
+
+  module Gen(C: COLL)
+      (Update: functor(C:COLL with type Dom.kind = C.Dom.kind)
+               -> sig val update : C.Dom.v -> C.Dom.v end) =
+    struct
+      module U = Update(C)
+      let foo = U.update(C.Dom.z)
+    end
+end;;
+
+open GEF
+open Doms
+
+module Test = Gen(GColl(FDomain))(DivisionUpdate);;
+let test = Test.foo;;
+
+module C_F = GColl(FDomain);;
+module Test1 = Gen(C_F)(DivisionUpdate);;
+
+module C_I = GColl(IDomain);;
+module Test2 = Gen(C_I)(DivisionUpdate);; (* reports an error *)
+
