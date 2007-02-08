d19a7e482600e58f24a55c61a2dd9440d388aa06
Jacques Carette <carette@mcmaster.ca>
2007-02-08 20:08:39 +0000

    make sure that the tests when run give the 'right' result

--- matrix/incope.ml
+++ matrix/incope.ml
@@ -26,21 +26,21 @@ type ('a,'v) result = RL of 'v | RC of ('a,'v) code;;
 (* This class defines syntax (and its instances, semantics) of our language
  *)
 
-
-
 module type Symantics = sig
   type ('c,'v) repr
   val int  : int -> ('c,int) repr
   val add  : ('c,int) repr -> ('c,int) repr -> ('c,int) repr
-  (* The last two arguments to ifeq are functional terms.
-     One of them is applied to the first argument of ifeq.
-     The reason for this sharade is to prevent evaluation
-     of both arguments of ifeq in a CBV language.
-     ifeq must be a syntactic form rather than just a function.
+  val eql  : ('c,'v) repr -> ('c,'v) repr -> ('c,bool) repr
+  val bool : bool -> ('c,bool) repr
+  (* The last two arguments to if are functional terms.
+     One of them is applied to unit.
+     The reason for this charade is to prevent evaluation
+     of both arguments of if_ in a CBV language.
+     if_ must be a syntactic form rather than just a function.
      Or, at least it shouldn't take just (undelayed) terms.
   *)
-  val ifeq : ('c,int) repr -> ('c,int) repr ->
-             ('c,int->'a) repr -> ('c,int->'a) repr -> ('c,'a) repr 
+  val if_ : ('c,bool) repr ->
+             (unit -> ('c,'a) repr) -> ('c,unit->'a) repr -> ('c,'a) repr 
 
   val lam : (('c,'a) repr -> ('c,'b) repr) -> ('c,'a->'b) repr
   val app : ('c,'a->'b) repr -> ('c,'a) repr -> ('c,'b) repr
@@ -63,12 +63,12 @@ module EX(S: Symantics) = struct
 
  let testgib () = lam (fun x -> lam (fun y ->
                   fix (fun self -> lam (fun n ->
-                      ifeq n (int 0) (lam (fun _ -> x))
+                      if_ (eql n (int 0)) (fun _ -> x)
                         (lam (fun _ ->
-			  (ifeq n (int 1) (lam (fun _ -> y))
-			   (lam (fun _ ->
+                          (if_ (eql n (int 1)) (fun _ -> y)
+                           (lam (fun _ ->
                              (add (app self (add n (int (-1))))
-				(app self (add n (int (-2))))))))))))))
+                                  (app self (add n (int (-2))))))))))))))
 
 
  let testgib1 () = app (app (app (testgib ()) (int 1)) (int 1)) (int 5)
@@ -76,6 +76,7 @@ module EX(S: Symantics) = struct
  let test1r = get_res (test1 ())
  let test2r = get_res (test2 ())
  let test3r = get_res (test3 ())
+ let test3r' = get_res (test3 ())
 
  let testgibr = get_res (testgib ())
  let testgib1r = get_res (testgib1 ())
@@ -92,7 +93,9 @@ module R = struct
   type ('c,'v) repr = 'v    (* absolutely no wrappers *)
   let int (x:int) = x
   let add e1 e2 = e1 + e2
-  let ifeq ie1 ie2 et ee = if ie1 = ie2 then (et ie1) else (ee ie1)
+  let bool (b:bool) = b
+  let eql x y = x == y
+  let if_ eb et ee = if eb then (et ()) else (ee ())
 
   let lam f = f
   let app e1 e2 = e1 e2
@@ -101,14 +104,12 @@ module R = struct
   let get_res x = RL x
 end;;
 
-
 module EXR = EX(R);;
 let itest1 = EXR.test1r;;
 let itest2 = EXR.test2r;;
 let itest3 = EXR.test3r;;
 let itestg = EXR.testgibr;;
 let itestg1 = EXR.testgib1r;;
-
 
 (* ------------------------------------------------------------------------ *)
 (* Pure compiler *)
@@ -120,9 +121,11 @@ let itestg1 = EXR.testgib1r;;
 module C = struct
   type ('c,'v) repr = ('c,'v) code
   let int (x:int) = .<x>.
+  let bool (b:bool) = .<b>.
   let add e1 e2 = .<.~e1 + .~e2>.
-  let ifeq ie1 ie2 et ee = 
-    .<let i1 = .~ie1 in if i1 = .~ie2 then .~et i1 else .~ee i1>.
+  let eql x y = .< .~x == .~y >.
+  let if_ eb et ee = 
+    .<if .~eb then .~(et () ) else .~ee ()>.
 
   let lam f = .<fun x -> .~(f .<x>.)>.
   let app e1 e2 = .<.~e1 .~e2>.
@@ -139,13 +142,29 @@ let ctest3 = EXR.test3r;;
 let ctestg = EXR.testgibr;;
 let ctestg1 = EXR.testgib1r;;
 
+(* actually run some of the above tests *)
+let ctest2' = let res = match (ctest2) with
+    | (RL x) -> failwith "not what we want"
+    | (RC x) -> (.< .~x 5 >.) 
+in .! res;;
+let ctest3' = let f = (fun x -> x+17) in
+    let res = match ctest3 with
+    | (RL x) -> failwith "not what we want"
+    | (RC x) -> (.< .~x f >.) 
+in .! res;;
+let ctestg1' = 
+    let res = match ctestg1 with
+    | (RL x) -> failwith "not what we want"
+    | (RC x) -> x
+in .! res;;
+
 
 (*
 module P = struct
   type ('c,'v) repr = ('c,'v) code
   let int (x:int) = fun iint -> let x = iint x in .<x>.
   let add e1 e2 = fun iadd -> .<iadd .~e1 .~e2>.
-  let ifeq ie1 ie2 et ee = 
+  let if_ ie1 ie2 et ee = 
     .<let i1 = .~ie1 in if i1 = .~ie2 then .~et i1 else .~ee i1>.
 
   let lam f = .<fun x -> .~(f .<x>.)>.
