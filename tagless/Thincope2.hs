95c72777d9ee7930e322ff471f6a49950011ce76
Jacques Carette <carette@mcmaster.ca>
2012-08-15 19:53:17 +0000

    improve v2 somewhat

--- tagless/Thincope2.hs
+++ tagless/Thincope2.hs
@@ -5,30 +5,37 @@ module Thincope2 where
 
 import qualified Thincope as T
 
--- Now we go 'the other way around' where we extract the interpreter
--- from the compiler
+-- Below is a very torturous way to write down 'id'.
+-- This "derives" an interpreter (isomorphic to T.R) from T.C
+-- Note that all the lift functions go through the "compiler" C.
 
-newtype R a = R a deriving Show
-unR (R x) = x
+newtype S a = S a deriving Show
+unS (S x) = x
 
-instance T.Symantics R where
-    int = R
-    bool = R
+instance T.Symantics S where
+    int x = S $(T.lift1 id [|x|])
+    bool b = S $(T.lift1 id [|b|])
 
     -- lam f = R (unR . f . R)
-    lam f = R (let g = unR . f . R in $(T.liftfn1 T.lam [|g|]))
+    lam f = S (let g = unS . f . S in $(T.liftfn1 T.lam [|g|]))
     -- app e1 e2 = R( (unR e1) (unR e2) )
-    app (R e1) (R e2) = R $(T.lift2 T.app [|e1|] [|e2|])
+    app (S e1) (S e2) = S $(T.lift2 T.app [|e1|] [|e2|])
 
-    fix f = R( fx (unR . f . R)) where fx f = f (fx f)
+    fix f = S( fx (unS . f . S)) where fx f = f (fx f)
     -- This won't work because it is a 'functional' fix which is not
     -- what Symantics is declared to be.
-    -- fix f = R (let g = unR . f . R in
+    -- fix f = S (let g = unS . f . S in
     --           $(T.unC $ T.fix (\(T.C ) -> T.C [|g $x|])))
 
-    add (R e1) (R e2) = R $(T.lift2 T.add [|e1|] [|e2|])
-    mul (R e1) (R e2) = R $(T.lift2 T.mul [|e1|] [|e2|])
-    leq (R e1) (R e2) = R $(T.lift2 T.leq [|e1|] [|e2|])
-    if_ (R be) (R et) (R ee) = R $(T.lift3 T.if_ [|be|] [|et|] [|ee|])
+    add (S e1) (S e2) = S $(T.lift2 T.add [|e1|] [|e2|])
+    mul (S e1) (S e2) = S $(T.lift2 T.mul [|e1|] [|e2|])
+    leq (S e1) (S e2) = S $(T.lift2 T.leq [|e1|] [|e2|])
+    if_ (S be) (S et) (S ee) = S $(T.lift3 T.if_ [|be|] [|et|] [|ee|])
 
-compR = unR
+compS = unS
+
+test1 :: () -> S Int
+test1 = T.test1
+
+test2 :: () -> S (Int -> Int)
+test2 = T.test2
